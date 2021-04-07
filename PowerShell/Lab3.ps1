d1be41ae1ee6902631b3b087d59cd27f67b373f3
tony10-07 <200447384@student.georgianc.on.ca>
2021-04-07 19:10:28 -0400

    Lab3

---
+++ PowerShell/Lab3.ps1
@@ -0,0 +1,9 @@
+﻿Get-CimInstance Win32_NetworkAdapterConfiguration | 
+Where-Object IPEnabled -EQ True |
+Select-Object @{n="Adapter Description";e={$_.Description}},
+              Index,
+              IPAddress,
+              @{n="Subnet Mask";e={$_.IPSubnet}},
+              @{n="DNS Domain Name";e={$_.DNSHostname}},
+              @{n="DNS Server";e={$_.DNSServerSearchOrder}} | 
+Format-Table
